444a77279f3a5694c173a4db71ee796755e59372
Jorge Moratilla <jorge@moratilla.com>
2012-10-15 02:15:46 +0200

    Beautiful code, but POST doesn't work :-/

--- samples/ProxmoxAPITest.rb
+++ samples/ProxmoxAPITest.rb
@@ -1,49 +1,80 @@
+# Proxmox API Tests
+# 
+# Author: Jorge Juan Moratilla Porras
+# 
+# Description: some tests with the Proxmox VE API
+#
+#TODO: PUT
+
 require 'rubygems'
 require 'rest_client'
 require 'json'
 require 'pp'
-require 'base64'
+require 'colorize'
 
-# Sacar las variables a variables de entorno
+# Default values
 username = ENV['PVE_USERNAME'] || 'test'
 realm    = ENV['PVE_REALM'] || 'pve'
 password = ENV['PVE_USER_PASSWORD'] || 'test123'
-url_auth = ENV['PVE_CLUSTER_URL'] || 'https://localhost:8006/api2/json/access/ticket'
+url_base = ENV['PVE_CLUSTER_URL'] || 'https://localhost:8006/api2/json/'
+csrf_prevention_token = nil
+token = nil
 
-# Autenticacion
+# RestClient logger
+log = RestClient.log = []
 
-response_auth = RestClient.post url_auth,{:username=>username,:realm => realm, :password=>password}
-data = JSON.parse(response_auth.body)
-ticket = data['data']['ticket']
-csrf_prevention_token = data['data']['CSRFPreventionToken']
+site = RestClient::Resource.new(url_base)
 
-# Codificacion del ticket para convertirlo en algo como esto:
-# 'PVEAuthCookie=PVE%3Atest@pve%3A5079E676%3A%3AE5BtOz0UhhAWDKVSxPBpegcrcp/RzEdg4Q9oI9YmBvrycGD6st3iE6uTWAMviX2cDq5OlezVYgjtE8/v5EtjgblNjfT2jtCoNqZtpIyV/pKhwfqFw5S2bjYJH52jSlnbxvlDgc7cciX7lSTHvrZOpc0trKmElYWWfb68po6wV4obgckECBxFo0Gmh//DbM2dyAdtlSS23opZIg3gYgb3A7+rmE8v2injbAvqicGuF5ard9o1pRbkE4SW5RB8wJLquGYu7htAkpRgWKZCpBsArk+cLXp1Uq0+1PRm5Ufp4lN6hVkCgLMjWcPNy4pkitO/vKMvr5YpAmjBRw7HS2IA3Q%3D%3D'
-token = 'PVEAuthCookie=' + ticket.gsub!(/:/,'%3A').gsub!(/=/,'%3D')
+puts 'AUTH'.blue
 
-# Acceso a la API: GET
-url_status = 'https://localhost:8006/api2/json/cluster/resources'
-response_status = RestClient.get(
-  url_status,
-  {
-    :CSRFPreventionToken => csrf_prevention_token,
-    :cookie => token
-  }
-) { |response, request, result, &block|
-  case response.code
-  when 200
-    p "It worked !"
-    response.body
-  when 401
-    p "Error de Autenticacion"
-  else
-    response.return!(request, result, &block)
+print 'To request access: '.yellow
+site['access/ticket'].post :username=>username,:realm=>realm,:password=>password do |response, request, result, &block| 
+  if response.code == 200 then
+    data = JSON.parse(response.body)
+    ticket = data['data']['ticket']
+    csrf_prevention_token = data['data']['CSRFPreventionToken']
+    if !ticket.nil? then
+# Token is a cookie coded like this one
+# 'PVEAuthCookie=PVE%3Atest@pve%3A5079E676%3A%3AE5Btg[...]crcp/RzEitO/vKMvr5YpAmjBRw7HS2IA3Q%3D%3D'
+      token = 'PVEAuthCookie=' + ticket.gsub!(/:/,'%3A').gsub!(/=/,'%3D')
+    end
   end
-}
+  puts "#{response.code}" 
+end 
 
-#TODO: Acceso a la API: POST
+puts 'GET'.blue
 
-#TODO: Acceso a la API: PUT
+print 'To list cluster resources: '.yellow
+site['cluster/resources'].get :cookie => token do |response, request, result, &block|
+  puts "#{response.code}"
+end
 
-#TODO: Acceso a la API: DELETE
+print 'To list available templates for download: '.yellow
+site['nodes/ankh/aplinfo'].get :CSRFPreventionToken => csrf_prevention_token, :cookie => token do |response, request, result, &block|
+  puts "#{response.code}"
+end
 
+print 'To list all VM\'s on a node: '.yellow
+site['nodes/ankh/openvz'].get :CSRFPreventionToken => csrf_prevention_token, :cookie => token do |response, request, result, &block|
+  puts "#{response.code}"
+end
+
+puts 'POST'.blue
+
+print 'To download a template: '.yellow
+site['nodes/ankh/aplinfo'].post :CSRFPreventionToken => csrf_prevention_token, :cookie => token, :node => 'ankh', :storage => 'local', :template => 'ubuntu-10.04-turnkey-appengine_11.3-1_i386.tar.gz' do |response, request, result, &block|
+  puts "#{response.code}"
+end
+
+print 'To create an openvz VM: '.yellow
+site['nodes/ankh/openvz?node=ankh&vmid=401&ostemplate=local%3Avztmpl%2Fubuntu-11.10-x86_64-jorge2-.tar.gz'].post :CSRFPreventionToken=>csrf_prevention_token, :cookie=>token do |response, request, result, &block|
+  puts "#{response.code}"
+end
+
+puts 'DELETE'.blue
+print 'To destroy an existing openvz VM: '.yellow
+site['nodes/ankh/openvz/401'].delete :CSRFPreventionToken => csrf_prevention_token, :cookie => token do |response, request, result, &block|
+  puts "#{response.code}"
+end
+
+#pp log
