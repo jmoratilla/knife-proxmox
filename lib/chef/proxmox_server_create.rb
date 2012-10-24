9838d72f9504ae8ccdddd0cad58c409d6cbb1364
Jorge Moratilla <jorge@moratilla.com>
2012-10-24 03:00:03 +0200

    server create works.  Not perfect but functional

--- lib/chef/proxmox_server_create.rb
+++ lib/chef/proxmox_server_create.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'rest_client'
 require 'json'
+require 'cgi'
 
 class Chef
   class Knife
@@ -16,6 +17,7 @@ class Chef
             require 'rubygems'
             require 'rest_client'
             require 'json'
+            require 'cgi'
           end
 
           option :pve_cluster_url,
@@ -49,10 +51,45 @@ class Chef
             :proc  => Proc.new {|node| Chef::Config[:knife][:pve_node_name] = node }
           
           # TODO: parameters for openvz should be in other object
-          option :openvz_hostname,
+          option :vm_hostname,
             :short => "-N hostname",
-            :description => "OpenVZ hostname",
-            :proc  => Proc.new {|hostname| }
+            :description => "VM instance hostname"
+          
+          option :vm_cpus,
+            :long  => "--cpus number",
+            :description => "Number of cpus of the VM instance"
+            
+          option :vm_memory,
+            :long  => "--mem MB",
+            :description => "Memory in MB"
+            
+          option :vm_swap,
+            :long  => "--swap MB",
+            :description => "Memory in MB for swap"
+            
+          option :vm_vmid,
+            :long  => "--vmid id",
+            :description => "Id for the VM"
+            
+          option :vm_disk,
+            :long  => "--disk GB",
+            :description => "Disk space in GB"
+            
+          option :vm_storage,
+            :long  => "--storage name",
+            :description => "Name of the storage where to reserve space"
+            
+          option :vm_password,
+            :long  => "--vm_pass password",
+            :description => "root password for VM (openvz only)"
+            
+          option :vm_netif,
+            :long  => "--netif netif_specification",
+            :description => "description of the network interface (experimental)"
+            
+          option :vm_template,
+            :long  => "--template number",
+            :description => "id of the template"
 
       def run
         site = RestClient::Resource.new(Chef::Config[:knife][:pve_cluster_url])
@@ -68,22 +105,60 @@ class Chef
               if !ticket.nil? then
                 # Token is a cookie coded like this one
                 # 'PVEAuthCookie=PVE%3Atest@pve%3A5079E676%3A%3AE5Btg[...]crcp/RzEitO/vKMvr5YpAmjBRw7HS2IA3Q%3D%3D'
+                # TODO: change ticket.gsub for CGI.escape(str)
                 token = 'PVEAuthCookie=' + ticket.gsub!(/:/,'%3A').gsub!(/=/,'%3D')
               end
             end
         end
         auth_params = {:CSRFPreventionToken => csrf_prevention_token, :cookie => token}
 
-
-        # FIXME: vmid hay que conseguir obtener el siguiente disponible
-        # FIXME: storagename debe ser automatico
-        # FIXME: template debe ser accesible a traves de parametros
-        # FIXME: memoria, swap, cpus, disk, hostname, password, netif deben ser configurados a traves de parametros
-        site["nodes/#{Chef::Config[:knife][:pve_node_name]}/openvz"].post 'vmid=401&hostname=melon1&storage=local&password=melon123&ostemplate=local%3Avztmpl%2Fubuntu-11.10-x86_64-jorge2-.tar.gz&memory=512&swap=512&disk=4&cpus=1&netif=ifname%3Deth0%2Cbridge%3Dvmbr0', auth_params do |response, request, result, &block|
+        vm_id       = config[:vm_vmid]     || new_vmid(site,auth_params)
+        vm_hostname = config[:vm_hostname] || 'proxmox'
+        vm_storage  = config[:vm_storage]  || 'local'
+        vm_password = config[:vm_password] || 'pve123'
+        vm_cpus     = config[:vm_cpus]     || 1
+        vm_memory   = config[:vm_memory]   || 512
+        vm_disk     = config[:vm_disk]     || 4
+        vm_swap     = config[:vm_swap]     || 512
+        vm_netif    = config[:vm_netif]    || 'ifname%3Deth0%2Cbridge%3Dvmbr0'
+        vm_template = template_number_to_name(site,auth_params,config[:vm_template],vm_storage) || 'local%3Avztmpl%2Fubuntu-11.10-x86_64-jorge2-.tar.gz'
+        
+        vm_definition = "vmid=#{vm_id}&hostname=#{vm_hostname}&storage=#{vm_storage}&password=#{vm_password}&ostemplate=#{vm_template}&memory=#{vm_memory}&swap=#{vm_swap}&disk=#{vm_disk}&cpus=#{vm_cpus}&netif=#{vm_netif}"
+        ui.msg vm_definition
+        
+        site["nodes/#{Chef::Config[:knife][:pve_node_name]}/openvz"].post "#{vm_definition}", auth_params do |response, request, result, &block|
           ui.msg("Result: #{response.code}")
         end
 
       end
+      
+      def new_vmid(connection,auth)
+        vmid ||= connection['cluster/resources?type=vm'].get auth do |response, request, result, &block|
+          data = JSON.parse(response.body)['data']
+          vmids = Set[]
+          data.each {|entry|
+            vmids.add entry['vmid']
+          }
+          vmids.max + 1
+        end
+      end
+      
+      #TODO: la salida debe estar codificada al estilo 
+      # 'local%3Avztmpl%2Fubuntu-11.10-x86_64-jorge2-.tar.gz'
+      
+      def template_number_to_name(connection,auth,number,storage)
+        template_list = []
+        #TODO: esta parte hay que sacarla a un modulo comun de acceso a templates
+        connection["nodes/#{Chef::Config[:knife][:pve_node_name]}/storage/#{storage}/content"].get auth do |response, request, result, &block|
+          JSON.parse(response.body)['data'].each { |entry|
+            if entry['content'] == 'vztmpl' then
+              template_list << entry['volid']
+            end
+          }
+        end
+        return CGI.escape(template_list[number.to_i])
+      end
+      
     end
   end
 end
